38c09f57efee8e72ae6176295d6279f89db3bc3a
Ted Moens <ted.moens@gmail.com>
2018-10-16 12:16:53 -0700

    switch production to use https

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,10 +1,10 @@
-export const environment = {
+exporexport const environment = {
   production: true,
   oktaEnv: {
     issuer: 'https://dev-574317.oktapreview.com/oauth2/default',
-    redirectUri: 'http://rogersrankings.com/TCStatsAdminClient/implicit/callback',
+    redirectUri: 'https://rogersrankings.com/TCStatsAdminClient/implicit/callback',
     clientId: '0oaf4igrrdiZLXT1Q0h7',
     scope: 'openid email phone groups',
   },
-  serverPrefix: 'http://rogersrankings.com/stats'
+  serverPrefix: 'https://rogersrankings.com/stats'
 };
